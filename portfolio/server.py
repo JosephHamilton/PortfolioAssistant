cf519e50fb85e38b6fdc76b1a2540bd89f034b4a
Joe Hamilton <jhhamilton1640@gmail.com>
2019-09-04 17:02:02 -0400

    Adding buttons to add positions and sell positions

--- portfolio/server.py
+++ portfolio/server.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, make_response, render_template, flash, redirect, url_for
 from wtforms import Form, SubmitField, TextField, validators, IntegerField, DecimalField
 from DBHandler import DBHandler
-import netifaces as ni
-from zeroconf import ServiceInfo, Zeroconf
-import socket
 import random
-import pickle
+
 
 class RegisterForm(Form):
     symbol = TextField('Stock Symbol', [validators.DataRequired(), validators.Length(max=50)])
@@ -23,9 +20,14 @@ db = DBHandler()
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/portfolio", methods=['GET', 'POST'])
-def cart():
-
+def view_portfolio():
     try:
+        if request.method == "POST":
+            if request.form["submit_button"] == "add_position":
+                return redirect(url_for('add_position'))
+            elif request.form["submit_button"] == "shop":
+                return redirect(url_for('sell_position'))
+
         total = 0
 
         return render_template("portfolio.html", items=None, total=total, cartID=None)
@@ -35,11 +37,30 @@ def cart():
         return str("help")
 
 
-if __name__ == "__main__":
-    # Find ip address to broadcast with zeroconf
-    # ipAddress = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+@app.route("/add_position", methods=["GET", "POST"])
+def add_position():
+    try:
+        form = RegisterForm(request.form)
 
-    # Create register qr code
-    # server_ip = ipAddress
-    
+        if request.method == "POST" and form.validate():
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            salt = random.randint(1000, 9999)
+            id_num = abs(hash(first_name + last_name + str(salt))) % (10 ** 8)
+
+            db.add_user(id_num, first_name, last_name)
+
+            print("First Name:{}\nLast Name:{}\nID Number:{}".format(first_name, last_name, id_num))
+
+            return redirect(url_for('register_rfid', memberID=id_num))
+        elif not form.validate():
+            flash('Error: All the form fields are required. ')
+
+        return render_template("register.html", form=form)
+
+    except Exception as e:
+        return str(e)
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
